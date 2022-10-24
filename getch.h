b1ccde8f70be4883e2547af8f7c9e4c037942261
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-10-24 03:13:58 +0200

    Add comments

--- getch.h
+++ getch.h
@@ -6,13 +6,17 @@
 
 char getch() {
   char getch_input = 0;
+  // Break loop when key was pressed.
   while (getch_input == 0) {
     for (char i = 97; i < 122; i++) {
+      // Check for a-z keys press.
       if (GetAsyncKeyState(toupper(i))) {
+        // Break loop when key was released.
         while (1) {
           if (!GetAsyncKeyState(toupper(i))) {
+            // Assign pressed key to getch_input variable.
             getch_input = i;
-            // Uppercase
+            // If shift was pressed make char uppercase(e.g. a -> A).
             if (GetAsyncKeyState(VK_SHIFT)) {
               getch_input -= 32;
             }
@@ -22,6 +26,7 @@ char getch() {
       }
     }
   }
+  // Return pressed char.
   return getch_input;
 }
 
